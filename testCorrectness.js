08276c9a39f07c2614db9cdcdd580382b0219dd1
Erik <erik.safford@wsu.edu>
2018-10-09 12:20:08 -0700

    Finished cumulativeSum tests, edited primeGen tests

--- testCorrectness.js
+++ testCorrectness.js
@@ -1,68 +1,37 @@
+/* Test Suite 1 - Correctness */
+
+// ---------------------------------------------------------------------------------------------------------------------
 /* eslint-env mocha, chai */
 /* global chai, primeGen */
 
-describe('primeGen(10) must return primes below 10, [2,3,5,7]', function () {
-  it('returns 2 as 1st prime', function () {
-    let primeArray = [];
-    primeArray = primeGen(10); // primeArray should return an array containing [2,3,5,7]
-    chai.expect(primeArray[0]).to.equal(2);
-  });
-  it('returns 3 as 2nd prime', function () {
+// primeGen() test cases
+describe('primeGen', function () {
+  it('primeGen(10) = [2, 3, 5, 7]', function () {
     let primeArray = [];
     primeArray = primeGen(10);
-    chai.expect(primeArray[1]).to.equal(3);
+    chai.expect(primeArray).to.have.same.members([2, 3, 5, 7]);
   });
-  it('returns 5 as 3rd prime', function () {
+  it('primeGen(20) = [2, 3, 5, 7, 11, 13, 17, 19]', function () {
     let primeArray = [];
-    primeArray = primeGen(10);
-    chai.expect(primeArray[2]).to.equal(5);
-  });
-  it('returns 7 as 4th prime', function () {
-    let primeArray = [];
-    primeArray = primeGen(10);
-    chai.expect(primeArray[3]).to.equal(7);
+    primeArray = primeGen(20);
+    chai.expect(primeArray).to.have.same.members([2, 3, 5, 7, 11, 13, 17, 19]);
   });
 });
 
-describe('primeGen(20) must return primes below 20, [2,3,5,7,11,13,17,19]', function () {
-  it('returns 2 as 1st prime', function () {
-    let primeArray = [];
-    primeArray = primeGen(20); // primeArray should return an array containing [2,3,5,7,11,13,17,19]
-    chai.expect(primeArray[0]).to.equal(2);
+// ---------------------------------------------------------------------------------------------------------------------
+/* eslint-env mocha, chai */
+/* global chai, cumulativeSum */
+
+// cumulativeSum() test cases
+describe('cumulativeSum', function () {
+  it('cumulativeSum([1, 2, 3, 4]) = [1, 3, 6, 10]', function () {
+    let sumArray = [];
+    sumArray = cumulativeSum([1, 2, 3, 4]);
+    chai.expect(sumArray).to.have.same.members([1, 3, 6, 10]);
   });
-  it('returns 3 as 2nd prime', function () {
-    let primeArray = [];
-    primeArray = primeGen(20);
-    chai.expect(primeArray[1]).to.equal(3);
-  });
-  it('returns 5 as 3rd prime', function () {
-    let primeArray = [];
-    primeArray = primeGen(20);
-    chai.expect(primeArray[2]).to.equal(5);
-  });
-  it('returns 7 as 4th prime', function () {
-    let primeArray = [];
-    primeArray = primeGen(20);
-    chai.expect(primeArray[3]).to.equal(7);
-  });
-  it('returns 11 as 5th prime', function () {
-    let primeArray = [];
-    primeArray = primeGen(20); // primeArray should return an array containing [2,3,5,7,11,13,17,19]
-    chai.expect(primeArray[4]).to.equal(11);
-  });
-  it('returns 13 as 6th prime', function () {
-    let primeArray = [];
-    primeArray = primeGen(20);
-    chai.expect(primeArray[5]).to.equal(13);
-  });
-  it('returns 17 as 7th prime', function () {
-    let primeArray = [];
-    primeArray = primeGen(20);
-    chai.expect(primeArray[6]).to.equal(17);
-  });
-  it('returns 19 as 8th prime', function () {
-    let primeArray = [];
-    primeArray = primeGen(20);
-    chai.expect(primeArray[7]).to.equal(19);
+  it('cumulativeSum([10, 11, 12, 13, 14]) = [10, 21, 33, 46, 60]', function () {
+    let sumArray = [];
+    sumArray = cumulativeSum([10, 11, 12, 13, 14]);
+    chai.expect(sumArray).to.have.same.members([10, 21, 33, 46, 60]);
   });
 });
